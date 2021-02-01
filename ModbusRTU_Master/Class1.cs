1a964030b03097030dd2d2c71f793dc8de37c8d9
johnny <kz85430@gmail.com>
2021-02-01 14:41:27 +0800

    加入專案檔案。

---
+++ ModbusRTU_Master/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Modbus;
+using Modbus.Device;
+using Modbus.Data;
+using Modbus.Message;
+using System.IO.Ports;  //for serial port
+//tcp
+using System.Net;         //for tcp client
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+namespace ModbusRTU_Master
+{
+    public class PCS_delta_100kw
+    {
+        public void pcs_read_AFC(ModbusSerialMaster master)
+        {
+            try
+            {
+                master.ReadHoldingRegisters(1, 5395, 2);
+            }
+            catch (Exception ee)
+            {
+                Debug.Print(ee.Message);
+            } 
+        }
+    }
+        class Class1
+    {
+    }
+}
